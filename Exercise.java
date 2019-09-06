25b734e37d2ab4fabebc7d2ff926601f7b4e93b7
Wong Junon <adjadas1@hotmail.com>
2019-09-06 15:54:27 +0800

    java

---
+++ Exercise.java
@@ -0,0 +1,28 @@
+{
+		int randomNum = (int)(Math.random()*3);         //让电脑生成随机的剪刀石头布
+		System.out.printIn("scissor (0), rock (1), paper (2): ");
+		Scanner input = new Scanner(System.in);
+		int userNum = input.nextInt();
+		
+		
+		
+}
+function game(int x, int y)
+{
+		if (x == 0 && y == 0)
+		{
+			System.out.printIn("The computer is scissor. You are scissor too. It is a draw");
+		}
+		else if (x == 0 && y == 1)
+		{
+			System.out.printIn("The computer is scissor. You are rock. You won");
+		}
+		else if (x == 0 && y == 2)
+		{
+			System
+		}
+}	
+function loop()
+{
+	
+｝
